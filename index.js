0b1c7b880b1223582c69354dd57ebaed6c7930e7
IshaTamrakar <61106634+IshaTamrakar@users.noreply.github.com>
2023-02-26 21:35:53 +0530

    26/02/23

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 //     alert('doc working')
 // });
 
-console.log('doc working')
-var countryNameGenerator = require("country-name-generator")
-console.log(countryNameGenerator.firstName(false,'ma'));
+// console.log('doc working')
+// const countryNameGenerator = require("country-name-generator")
+// console.log(countryNameGenerator.firstName(false,'ma'));
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(request,response){
+    console.log(request);
+    response.send("hello world");
+});
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000");
+})
