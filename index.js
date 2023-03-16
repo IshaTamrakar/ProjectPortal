8a59b4b602313b9b437c545ce6aeba8b060b93da
IshaTamrakar <61106634+IshaTamrakar@users.noreply.github.com>
2023-03-16 23:50:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-// $(document).ready(function(){
-//     alert('doc working')
-// });
 
-// console.log('doc working')
-// const countryNameGenerator = require("country-name-generator")
-// console.log(countryNameGenerator.firstName(false,'ma'));
 
 const express = require("express");
-
 const app = express();
+
+//to parse information that we send from post request.
+app.use(express.urlencoded({extended: true})) //parse data that comes from html form
+app.use(express.json()) //to parse the incoming requests with json payloads
 
 app.get("/", function(request,response){
     console.log(request);
     response.send("hello world");
 });
+
+app.post("/", function(req,res){
+    res.send("It is working");
+})
 
 app.listen(3000, function(){
     console.log("server is running on port 3000");
